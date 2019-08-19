95f90e0752b6c567eb0b51f7261283ff908492bc
Nathan Peterson <natperson@gmail.com>
2019-08-19 12:48:39 -0700

    Updates

---
+++ precache-manifest.af843c487509f7ed54744bd0dfbb1b49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1290a0a0462f115dbcc91e81bc7b8f6e",
+    "url": "/seattle/index.html"
+  },
+  {
+    "revision": "fb7faff7b4e42cab2080",
+    "url": "/seattle/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "02fcd41e906499ada45e",
+    "url": "/seattle/static/js/2.a3d75798.chunk.js"
+  },
+  {
+    "revision": "fb7faff7b4e42cab2080",
+    "url": "/seattle/static/js/main.6afee02d.chunk.js"
+  },
+  {
+    "revision": "b630a010d99a4d8fde42",
+    "url": "/seattle/static/js/runtime~main.0edbcb4a.js"
+  }
+]);
